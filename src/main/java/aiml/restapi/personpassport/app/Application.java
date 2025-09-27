7304c41b1574031cccb8db473dfe478e61b2e643
Satish Paidikondala <113538629+satishpadikondala@users.noreply.github.com>
2025-09-27 13:22:29 +0530

    Added Spring Boot project files

---
+++ src/main/java/aiml/restapi/personpassport/app/Application.java
@@ -0,0 +1,13 @@
+package aiml.restapi.personpassport.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/aiml/restapi/personpassport/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package aiml.restapi.personpassport.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
